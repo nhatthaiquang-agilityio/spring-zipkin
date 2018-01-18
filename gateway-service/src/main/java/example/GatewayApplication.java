322e183fad053c40e3681897c6cbcc2f06c6b834
Nhat Thai <nhat.thaiquang@asnet.com.vn>
2018-01-18 11:25:18 +0700

    Add gateway

---
+++ gateway-service/src/main/java/example/GatewayApplication.java
@@ -0,0 +1,21 @@
+package example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import example.filters.SimpleFilter;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    @Bean
+    public SimpleFilter simpleFilter() {
+        return new SimpleFilter();
+    }
+}

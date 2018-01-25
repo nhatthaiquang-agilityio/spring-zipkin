8d7ecdc560fd604c1a6d8da6f0d58d30d030bc19
Nhat Thai <nhat.thaiquang@asnet.com.vn>
2018-01-25 09:57:14 +0700

    Update student-service

--- student-service/src/main/java/example/StudentApplication.java
+++ student-service/src/main/java/example/StudentApplication.java
@@ -3,10 +3,18 @@ package example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@RestController
 public class StudentApplication {
+
+    @GetMapping(value = "/available")
+    public String available() {
+        return "Student service";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(StudentApplication.class, args);

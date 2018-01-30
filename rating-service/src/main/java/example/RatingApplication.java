32c3fd3d903ce195111a289997db2ad47958354f
Nhat Thai <nhat.thaiquang@asnet.com.vn>
2018-01-30 11:00:39 +0700

    add dependency for rating service

--- rating-service/src/main/java/example/RatingApplication.java
+++ rating-service/src/main/java/example/RatingApplication.java
@@ -5,18 +5,31 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 @EnableDiscoveryClient
 @SpringBootApplication
 public class RatingApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(RatingApplication.class);
 
     public static void main(String[] args) {
-        SpringApplication.run(RatingApplication.class, args);
+        ConfigurableApplicationContext ctx = SpringApplication.run(RatingApplication.class, args);
+        LOG.info("Connected to RabbitMQ at: {}", ctx.getEnvironment().getProperty("spring.rabbitmq.host"));
     }
 
     @GetMapping(value = "/available")
     public String available() {
         return "Spring Rating service";
     }
+
+    @GetMapping(value = "/error11")
+    public String error11() {
+        LOG.error("Error request");
+        return "error 11";
+    }
+
 }
